284bcd844953b423be69f7babf7690e2cd551820
Sean Pinegar <spinegar@gmail.com>
2013-09-10 02:24:44 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,120 @@
+SugarCRM REST API Wrapper Class For SugarCRM 7
+=================================================
+
+License: MIT
+
+
+Contents
+--------
+1. About
+2. Installation
+3. Usage Example
+
+
+1.About
+-------
+- PHP wrapper class for interacting with a SugarCRM 7 REST API
+- Designed to work with SugarCRM 7 and the v10 REST API
+
+2. Installation via Composer
+----------------------------
+Edit composer.json
+
+	{
+		"repositories": [
+			{
+				"type": "vcs",
+				"url": "https://github.com/spinegar/sugarcrm7-api-wrapper-class"
+			}
+		],
+		"require": {
+			"spinegar/sugar7wrapper": "dev-master"
+		},
+		"minimum-stability": "dev"
+	}
+
+Then install with composer
+
+	$ composer install
+
+
+
+3.Usage Examples
+---------------
+
+	/* Instantiate and authenticate */
+	$sugar = new \Spinegar\Sugar7Wrapper\Rest();
+
+	$sugar->setUrl('https://sugar/rest/v10/');
+	$sugar->setUsername('restUser');
+	$sugar->setPassword('password');
+
+	$sugar->connect();
+
+	/* Retrieve all records in the Cases module */
+	$sugar->search('Cases');
+
+	/* Retrieve all records in the Cases module where the name = 'Case Name1' or 'Case Name2' */
+	$sugar->search('Cases', array(
+		'q' => '"Case1 Name" "Case2 Name"'
+	)); 
+
+	/* Retrieve the name field for all records in the Cases module */
+	$sugar->search('Cases', array(
+		'fields' => 'name'
+	)); 
+
+	/* Retrieve a specific record from the Cases module */
+	$sugar->retrieve('Cases', $record_id);
+
+	/* Create a case */
+	$sugar->create('Cases', array(
+		'name' => 'Case Name',
+		'status' => 'Assigned'
+	));
+
+	/* Update a case */
+	$sugar->update('Cases', $record_id, array(
+	    	'status' => 'Closed'
+	));
+
+	/* Favorite a case */
+	$sugar->favorite('Cases', $record_id);
+
+	/* Unfavorite a case */
+	$sugar->unfavorite('Cases', $record_id);
+
+	/* Retrieve cases related to an account */
+	$sugar->related('Accounts', $record_id, 'cases');
+
+	/* Relate a case to an account */
+	$sugar->relate('Accounts', $record_id, 'cases', $related_record_id);
+
+	/*Relate a contact to an opportunity and set relationship data */
+	$sugar->relate('Opportunities', $record_id, 'contacts', $related_record_id, array(
+		'contact_role' => 'Influencer'
+	))
+
+	/* Delete relationship between an account and case */
+	$sugar->unrelate('Accounts', $record_id, 'cases', $related_record_id);
+
+	/* Update relationship data */
+	$sugar->updateRelationship('Opportunities', $record_id, 'contacts', $related_record_id, array(
+		'contact_role' => 'Influencer'
+	))
+
+	/* Retrieve all attachments for a case */
+	$attachments = $sugar->related('Cases', $record_id, 'notes');
+
+	foreach($attachments['records'] as $attachment)
+	{
+		$output[] = $sugar->files('Notes', $attachment['id'])
+	}
+
+	return $output;
+
+	/* Delete the file associated to the filename field of a note */
+	$sugar->deleteFile('Notes', $record_id, 'filename')
+
+	/* Download  the file associated to the filename field of a note to the server */
+	$sugar->file('Notes', $record_id, 'filename', '/path/to/destination');
---
+++ src/Spinegar/Sugar7Wrapper/Rest.php
@@ -0,0 +1,464 @@
+<?php namespace Spinegar\Sugar7Wrapper;
+
+use Guzzle\Http\Client;
+use Guzzle\Common\Event;
+use Guzzle\Http\Query;
+
+/**
+ * SugarCRM 7 REST API Class
+ *
+ * @package   Sugar7Wrapper
+ * @category  Libraries
+ * @author  Sean Pinegar
+ * @license MIT License
+ * @link    https://github.com/spinegar/sugar7wrapper
+ */
+
+class Rest {
+
+  /**
+  * Variable: $url
+  * Description:  The URL of the SugarCRM REST API
+  * Example:  https://sugar/rest/v10/
+  */
+  private $url = null;
+
+  /**
+  * Variable: $username
+  * Description:  A SugarCRM User. 
+  */
+  private $username = null;
+
+  /**
+  * Variable: $password
+  * Description:  The password for the $username SugarCRM account
+  */
+  private $password = null;
+
+  /**
+  * Variable: $token
+  * Description:  OAuth 2.0 token
+  */
+  private $token = null;
+
+  /**
+  * Variable: $client
+  * Description:  Guzzle Client
+  */
+  public $client = null;
+
+  /**
+  * Variable: $authenticated
+  * Description:  Tells us if we are authenticated or not
+  */
+  public $authenticated = false;
+
+  /**
+  * Function: __destruct()
+  * Parameters:   none    
+  * Description:  OAuth2 Logout
+  * Returns:  TRUE on success, otherwise FALSE
+  */
+  function __destruct()
+  {
+    if(!$this->client)
+      return false;
+
+    $request = $this->client->post('oauth2/logout');
+    $request->setHeader('OAuth-Token', $this->token);    
+    $result = $request->send()->json();
+
+    return $result;
+  }
+  
+  /**
+  * Function: connect()
+  * Parameters:   none    
+  * Description:  Authenticate and set the oAuth 2.0 token
+  * Returns:  TRUE on login success, otherwise FALSE
+  */
+  function connect()
+  {
+    $this->client = new Client($this->url);
+
+    $request = $this->client->post('oauth2/token', null, array(
+        'grant_type' => 'password',
+        'client_id' => 'sugar',
+        'username' => $this->username,
+        'password' => $this->password,
+    ));
+
+    $results = $request->send()->json();
+   
+    if(!$results['access_token'])
+      return false;
+
+    $this->token = $results['access_token'];
+
+    $this->client->getEventDispatcher()->addListener('request.before_send', function(Event $event) {
+      $event['request']->setHeader('OAuth-Token', $this->token);
+    });
+
+    $this->authenticated = true;
+    
+    return true;
+  }
+
+  /**
+  * Function: getUrl()
+  * Parameters:   none    
+  * Description:  Get the SugarCRM REST API URL
+  * Returns:  returns $url
+  */
+  function getUrl()
+  {
+    return $this->url;
+  }
+
+  /**
+  * Function: setUrl()
+  * Parameters:   $value = URL for the REST API    
+  * Description:  Set $url
+  * Returns:  returns $url
+  */
+  function setUrl($value)
+  {
+    $this->url = $value;
+
+    return $this->url;
+  }
+
+  /**
+  * Function: setUsername()
+  * Parameters:   $value = Username for the REST API User    
+  * Description:  Set $username
+  * Returns:  returns $username
+  */
+  function setUsername($value)
+  {
+    $this->username = $value;
+
+    return $this->username;
+  }
+
+  /**
+  * Function: setPassword()
+  * Parameters:   none    
+  * Description:  Set $password
+  * Returns:  returns $passwrd
+  */
+  function setPassword($value)
+  {
+    $this->password = $value;
+
+    return $this->password;
+  }
+
+  /**
+  * Function: create()
+  * Parameters:   $module = Record Type
+  *   $fields = Record field values    
+  * Description:  This method creates a new record of the specified type
+  * Returns:  returns Array if successful, otherwise FALSE
+  */
+  function create($module, $fields)
+  {
+    $request = $this->client->post($module, null, $fields);
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: search()
+  * Parameters:  $module - The module to work with
+  *   $params = [
+  *     q - Search the records by this parameter, if you don't have a full-text search engine enabled it will only search the name field of the records.  (Optional)
+  *     maxResult - A maximum number of records to return Optional
+  *     offset -  How many records to skip over before records are returned (Optional)
+  *     fields -  Comma delimited list of what fields you want returned. The field date_modified will always be added  (Optional)
+  *     order_by -  How to sort the returned records, in a comma delimited list with the direction appended to the column name after a colon. Example: last_name:DESC,first_name:DESC,date_modified:ASC (Optional)
+  *     favorites - Only fetch favorite records (Optionall)
+  *     deleted - Show deleted records in addition to undeleted records (Optional)
+  *   ]
+  * Description:  Search records in this module
+  * Returns:  returns Object if successful, otherwise FALSE
+  */
+  function search($module, $params = array())
+  {
+    // return $params;
+    $request = $this->client->get($module);
+
+    $query = $request->getQuery();
+    foreach($params as $key=>$value)
+    {
+      $query->add($key, $value);
+    }
+
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: delete()
+  * Parameters: $module = Record Type
+  *   $record = The record to delete
+  * Description:  This method deletes a record of the specified type
+  * Returns:  returns Object if successful, otherwise FALSE
+  */
+  function delete($module, $record)
+  {
+    $request = $this->client->delete($module . '/' . $record);
+    $result = $request->send();
+
+    if(!$result)
+      return false;
+
+    return true;
+  }
+
+  /**
+  * Function: retrieve()
+  * Parameters: $module = Record Type
+  *   $record = The record to retrieve
+  * Description:  This method retrieves a record of the specified type
+  * Returns:  Returns a single record
+  */
+  function retrieve($module, $record)
+  {
+    $request = $this->client->get($module . '/' . $record);
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: update()
+  * Parameters: $module = Record Type
+  *   $record = The record to update
+  *   $fields = Record field values
+  * Description:  This method updates a record of the specified type
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function update($module, $record, $fields)
+  {
+    $request = $this->client->put($module . '/' . $record, null, json_encode($fields));
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: favorite()
+  * Parameters: $module = Record Type
+  *   $record = The record to favorite
+  * Description:  This method favorites a record of the specified type
+  * Returns:  Returns TRUE if successful, otherwise FALSE
+  */
+  function favorite($module, $record)
+  {
+    $request = $this->client->put($module . '/' . $record . '/favorite');
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: unfavorite()
+  * Parameters: $module = Record Type
+  *   $record = The record to unfavorite
+  * Description:  This method unfavorites a record of the specified type
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function unfavorite($module, $record)
+  {
+    $request = $this->client->delete($module . '/' . $record . '/favorite');
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: files()
+  * Parameters: $module = Record Type
+  *   $record = The record  we are working with
+  * Description:  Gets a listing of files related to a field for a module record.
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function files($module, $record)
+  {
+    $request = $this->client->get($module . '/' . $record . '/file');
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: file()
+  * Parameters: $module = Record Type
+  *   $record = The record  we are working with
+  *   $field = Field associated to the file
+  * Description:  Gets the contents of a single file related to a field for a module record.
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function file($module, $record, $field, $destination)
+  {
+    $file = $this->getUrl() . $module . '/' . $record . '/file/' . $field;
+    $request = $this->client->get($module . '/' . $record . '/file/' . $field );
+    $request->setResponseBody($destination);
+    $result = $request->send();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: saveFile()
+  * Parameters: $module = Record Type
+  *   $record = The record  we are working with
+  *   $params = [
+  *     format - sugar-html-json (Required),
+  *     delete_if_fails - Boolean indicating whether the API is to mark related record deleted if the file upload fails.  Optional (if used oauth_token is also required)
+  *     oauth_token - oauth_token_value Optional (Required if delete_if_fails is true)
+  *   ]
+  * Description:  Saves a file. The file can be a new file or a file override.
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function saveFile($module, $record, $field, $path, $params=array())
+  {
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    $contentType = finfo_file($finfo, $path);
+    finfo_close($finfo);
+
+    $request = $this->client->post($module . '/' . $record . '/file/' . $field, array(), $params);
+    $request->addPostFile(basename($path), dirname($path), $contentType)
+                  ->send();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: deleteFile()
+  * Parameters: $module = Record Type
+  *   $record = The record  we are working with
+  *   $field = Field associated to the file
+  * Description:  Saves a file. The file can be a new file or a file override.
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function deleteFile($module, $record, $field)
+  {
+    $request = $this->client->delete($module . '/' . $record . '/file/' . $field);
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: related()
+  * Parameters: $module = Record Type
+  *   $record = The record we are working with
+  *   $link = The link for the relationship
+  * Description:  This method retrieves a list of records from the specified link
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function related($module, $record, $link)
+  {
+    $request = $this->client->get($module . '/' . $record . '/link/' . $link);
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: relate()
+  * Parameters: $module = Record Type
+  *   $record = The record we are working with
+  *   $link = The link for the relationship
+  *   $related_record = the record to relate to
+  *   $fields = Relationship data
+  * Description:  This method relates 2 records
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function relate($module, $record, $link, $related_record, $fields)
+  {
+    $request = $this->client->post($module . '/' . $record . '/link/' . $link . '/' . $related_record, array(), $fields);
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: unrelate()
+  * Parameters: $module = Record Type
+  *   $record = The record to unfavorite
+  * Description:  This method removes the relationship for 2 records
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function unrelate($module, $record, $link, $related_record)
+  {
+    $request = $this->client->delete($module . '/' . $record . '/link/' . $link . '/' . $related_record);
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+
+  /**
+  * Function: updateRelationship()
+  * Parameters: $module = Record Type
+  *   $record = The record we are working with
+  *   $link = The link for the relationship
+  *   $related_record = the record to relate to
+  *   $fields = Relationship data
+  * Description:  This method updates relationship data
+  * Returns:  Returns an Array if successful, otherwise FALSE
+  */
+  function updateRelationship($module, $record, $link, $related_record, $fields)
+  {
+    $request = $this->client->put($module . '/' . $record . '/link/' . $link . '/' . $related_record, array(), json_encode($fields));
+    $result = $request->send()->json();
+
+    if(!$result)
+      return false;
+
+    return $result;
+  }
+}
